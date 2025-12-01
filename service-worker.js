f48368d051c250e1892dc2fd50cca42832e1ef92
monitoreocopla <monitoreo.copla@propuestadale.org>
2025-12-01 12:39:47 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="offline-saltos-v1";
+const CACHE="offline-saltos-v2";
 const FILES=["index.html","manifest.json","formulario.json","escuelas.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));});
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
